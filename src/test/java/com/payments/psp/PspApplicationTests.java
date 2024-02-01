78b294e02006b5473e44c35284dd2c35fcb8c0e1
Robert Pohl <robban.p@gmail.com>
2024-02-01 21:01:14 +0100

    init

---
+++ src/test/java/com/payments/psp/PspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payments.psp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
